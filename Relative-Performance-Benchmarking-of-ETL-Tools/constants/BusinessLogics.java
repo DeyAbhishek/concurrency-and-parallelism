0d721b0befe6ee474568ea3aa5e0402211462f81
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2014-05-30 04:45:39 -0400

    Create BusinessLogics.java

---
+++ Relative-Performance-Benchmarking-of-ETL-Tools/constants/BusinessLogics.java
@@ -0,0 +1,13 @@
+package constants;
+
+public class BusinessLogics {
+	
+	public static enum BusinessLogicImpl{
+		xmlparsing, variableformatparsing, mimickedbusinesslogic
+	}
+	
+	public static enum BusinessLogicNum{
+		single, multiple
+	}
+	
+}
