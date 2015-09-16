6bf161dc618440039aa2fc81dea2722f1ee972d9
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-09-16 14:10:22 -0400

    Update ExecutorServiceThreadPoolExample.java

--- ExecutorServiceThreadPoolExample.java
+++ ExecutorServiceThreadPoolExample.java
@@ -2,54 +2,24 @@
 
 import java.util.concurrent.*;
 public class ExecutorServiceThreadPoolExample {
-
- 
-
     public static void main(String args[]) {
-
        ExecutorService service = Executors.newFixedThreadPool(10);
-
        for (int i =0; i<20; i++){
-
            service.submit(new Task(i));
-
        }
        service.shutdown();
-
     }
-
-   
-
 }
 
- 
-
 final class Task implements Runnable{
-
     private int taskId;
-
-   
-
     public Task(int id){
-
         this.taskId = id;
-
     }
-
-   
-
     @Override
-
     public void run() {
-
-        System.out.println("Task ID : " + this.taskId +" performed by "
-
-                           + Thread.currentThread().getName());
-
+        System.out.println("Task ID : " + this.taskId +" performed by " + Thread.currentThread().getName());
     }
-
-   
-
 }
 
 
